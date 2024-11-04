4eeaad8011601e40661e7958b962ab64fcca2aa6
Olga Genier <genier.developer@gmail.com>
2024-11-04 17:00:34 +0500

    fix: add routing

--- src/entities/employee/model/employee-slice.ts
+++ src/entities/employee/model/employee-slice.ts
@@ -1,7 +1,12 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {EmployeeResponseData} from "./types.ts";
+
+export type EmployeeState = {
+  employees: EmployeeResponseData[],
+}
 
 export const employeeSlice = createSlice({
   name: "employee",
-  initialState: {},
+  initialState: {} as EmployeeState,
   reducers: {}
 })
