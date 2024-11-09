3f0abd4f31efa497faa05d4629d753000db97222
Olga Genier <genier.developer@gmail.com>
2024-11-09 19:35:38 +0500

    fix: bug

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -1,9 +1,11 @@
 import {Outlet} from 'react-router-dom';
+import {HomePage} from "../pages/home-page.tsx";
 
 export default function Root()  {
   return (
     <div>
-      <h1>Hello, there is an employee-app</h1>
+      <header>Hello, there is an employee-app</header>
+      <HomePage/>
       <Outlet/>
     </div>
   )
