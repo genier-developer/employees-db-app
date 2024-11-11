833c1a306b181c1da50ba01b121dad79428a321f
Olga Genier <genier.developer@gmail.com>
2024-11-11 19:21:42 +0500

    feat:

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,6 +1,8 @@
-import {useAppDispatch, useAppSelector} from "../app/store.ts";
+import {useAppDispatch, useAppSelector} from "../../app/store.ts";
 import {useEffect} from "react";
-import {fetchEmployees} from "../entities/employee/model/employee-slice.ts";
+import {fetchEmployees} from "../../entities/employee/model/employee-slice.ts";
+import {Checkbox} from "../../shared/ui/checkbox/checkbox.tsx";
+import s from './home.module.scss'
 
 export const HomePage = () => {
   const dispatch = useAppDispatch();
@@ -14,12 +16,13 @@ export const HomePage = () => {
   if(error) return <div>Error</div>
 
   return (
-    <div>
+    <div className={s.home}>
       {employees.map(employee => (
-        <div key={employee.id}>
+        <div key={employee.id} className={s.employee}>
           <h3>{employee.name}</h3>
           <p>{employee.role}</p>
           <p>{employee.phone}</p>
+          <Checkbox isArchive={employee.isArchive} />
         </div>
       ))}
     </div>
