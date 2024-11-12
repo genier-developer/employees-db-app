d9b944c3eab2fd6473c88c325e651f9e589db568
Olga Genier <genier.developer@gmail.com>
2024-11-12 19:14:19 +0500

    refactor:

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,42 +1,66 @@
-import {useAppDispatch, useAppSelector} from "../../app/store.ts";
-import {useEffect} from "react";
-import {employeeToggleArchiveStatus, fetchEmployees} from "../../entities/employee/model/employee-slice.ts";
-import {CheckboxUI} from "../../shared/ui/checkbox";
-import s from './home.module.scss'
+// import {useAppDispatch, useAppSelector} from "../../app/store.ts";
+// import {useEffect} from "react";
+// import {employeeToggleArchiveStatus, fetchEmployees} from "../../entities/employee/model/employee-slice.ts";
+// import {CheckboxUI} from "../../shared/ui/checkbox";
+// import s from './home.module.scss'
+// // import {EmployeeTable} from "../../shared/ui/employees-table";
+//
+// export const HomePage = () => {
+//   const dispatch = useAppDispatch();
+//   const {employees, loading, error} = useAppSelector(state => state.employee)
+//
+//   useEffect(()=>{
+//     dispatch(fetchEmployees())
+//   }, [dispatch])
+//
+//   const handleToggleStatus = (employeeId: number)=>{
+//     dispatch(employeeToggleArchiveStatus(employeeId))
+//
+//   }
+//
+//   if(loading) return <div>Loading...</div>
+//   if(error) return <div>Error</div>
+//
+//   return (
+//     <div className={s.home}>
+//       {employees.map(employee => (
+//         <div key={employee.id} className={s.employee}>
+//           <h3>{employee.name}</h3>
+//           <p>{employee.role}</p>
+//           <p>{employee.phone}</p>
+//           <p>{employee.birthday}</p>
+//           <CheckboxUI
+//             isArchive={employee.isArchive}
+//             onChangeChecked={()=>handleToggleStatus(employee.id)}
+//             label={'in archive'}
+//           />
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+//
 
-export const HomePage = () => {
+import { FC, useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/store';
+import { fetchEmployees } from '../../entities/employee/model/employee-slice';
+import { EmployeeTable } from '../../shared/ui/employees-table';
+
+export const HomePage: FC = () => {
   const dispatch = useAppDispatch();
-  const {employees, loading, error} = useAppSelector(state => state.employee)
+  const { loading, error } = useAppSelector((state) => state.employee);
 
-  useEffect(()=>{
-    dispatch(fetchEmployees())
-  }, [dispatch])
+  useEffect(() => {
+    dispatch(fetchEmployees());
+  }, [dispatch]);
 
-  const handleToggleStatus = (employeeId: number)=>{
-    dispatch(employeeToggleArchiveStatus(employeeId))
-
-  }
-
-  if(loading) return <div>Loading...</div>
-  if(error) return <div>Error</div>
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
 
   return (
-    <div className={s.home}>
-      {employees.map(employee => (
-        <div key={employee.id} className={s.employee}>
-          <h3>{employee.name}</h3>
-          <p>{employee.role}</p>
-          <p>{employee.phone}</p>
-          <p>{employee.birthday}</p>
-          <CheckboxUI
-            isArchive={employee.isArchive}
-            onChangeChecked={()=>handleToggleStatus(employee.id)}
-            label={'in archive'}
-          />
-        </div>
-      ))}
+    <div>
+      <EmployeeTable />
     </div>
-
-  )
-}
+  );
+};
 
