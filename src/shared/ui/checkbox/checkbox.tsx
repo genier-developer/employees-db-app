43f30bb6a6e9669f635ce4dd34e6c37901a9f33f
Olga Genier <genier.developer@gmail.com>
2024-11-13 12:06:09 +0500

    refactor: checkbox

--- src/shared/ui/checkbox/checkbox.tsx
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -1,27 +1,27 @@
-import Checkbox from '@mui/material/Checkbox';
-import {FormControlLabel} from "@mui/material";
-import {FC} from "react";
-import * as React from "react";
+  import Checkbox from '@mui/material/Checkbox';
+  import {FormControlLabel} from "@mui/material";
+  import {FC} from "react";
+  import * as React from "react";
 
-type CheckboxProps = {
-  onChangeChecked?: (checked: boolean) => void;
-  isArchive?: boolean;
-  label?: string;
-}
-
-export const CheckboxUI: FC<CheckboxProps> = ({label, onChangeChecked, isArchive}) => {
-  //wrapper for according to type of MUI Checkbox property
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if(onChangeChecked){
-      onChangeChecked(event.target.checked);
-    }
+  type CheckboxProps = {
+    onChangeChecked?: (checked: boolean) => void;
+    isArchive?: boolean;
+    label?: string;
   }
 
-  return (
-    <FormControlLabel
-      control={<Checkbox onChange={handleChange} checked={isArchive} />}
-      label={label}
-    />
-  );
-};
+  export const CheckboxUI: FC<CheckboxProps> = ({label, onChangeChecked, isArchive}) => {
+    //wrapper for according to type of MUI Checkbox property
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      if(onChangeChecked){
+        onChangeChecked(event.target.checked);
+      }
+    }
+
+    return (
+      <FormControlLabel
+        control={<Checkbox onChange={handleChange} checked={isArchive} />}
+        label={label}
+      />
+    );
+  };
 
