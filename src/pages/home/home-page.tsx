6e0e28c077a28ecaf800a01569cdc58a94b16957
Olga Genier <genier.developer@gmail.com>
2024-11-13 12:08:40 +0500

    refactor:

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,50 +1,9 @@
-// import {useAppDispatch, useAppSelector} from "../../app/store.ts";
-// import {useEffect} from "react";
-// import {employeeToggleArchiveStatus, fetchEmployees} from "../../entities/employee/model/employee-slice.ts";
-// import {CheckboxUI} from "../../shared/ui/checkbox";
-// import s from './home.module.scss'
-// // import {EmployeeTable} from "../../shared/ui/employees-table";
-//
-// export const HomePage = () => {
-//   const dispatch = useAppDispatch();
-//   const {employees, loading, error} = useAppSelector(state => state.employee)
-//
-//   useEffect(()=>{
-//     dispatch(fetchEmployees())
-//   }, [dispatch])
-//
-//   const handleToggleStatus = (employeeId: number)=>{
-//     dispatch(employeeToggleArchiveStatus(employeeId))
-//
-//   }
-//
-//   if(loading) return <div>Loading...</div>
-//   if(error) return <div>Error</div>
-//
-//   return (
-//     <div className={s.home}>
-//       {employees.map(employee => (
-//         <div key={employee.id} className={s.employee}>
-//           <h3>{employee.name}</h3>
-//           <p>{employee.role}</p>
-//           <p>{employee.phone}</p>
-//           <p>{employee.birthday}</p>
-//           <CheckboxUI
-//             isArchive={employee.isArchive}
-//             onChangeChecked={()=>handleToggleStatus(employee.id)}
-//             label={'in archive'}
-//           />
-//         </div>
-//       ))}
-//     </div>
-//   )
-// }
-//
-
 import { FC, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/store';
 import { fetchEmployees } from '../../entities/employee/model/employee-slice';
 import { EmployeeTable } from '../../shared/ui/employees-table';
+import {SelectUI} from "../../shared/ui/select";
+import {EmployeeFilter} from "../../features/employee-filter/ui/employee-filter.tsx";
 
 export const HomePage: FC = () => {
   const dispatch = useAppDispatch();
@@ -59,6 +18,8 @@ export const HomePage: FC = () => {
 
   return (
     <div>
+      <SelectUI />
+      <EmployeeFilter />
       <EmployeeTable />
     </div>
   );
